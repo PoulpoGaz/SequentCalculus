8c1baed07017610d80097f1e010319c6bf6bb73f
PoulpoGaz <valent.carrez@gmail.com>
2022-10-15 21:20:08 +0200

    parser now produces a treegit statusgit status doesn't support not...

--- src/main.ml
+++ src/main.ml
@@ -10,5 +10,20 @@
 *)
 let () =
   Printexc.record_backtrace true;
-  Parser.parse_formula "((a -> b) && c) || ((d && e) || f)";
-  print_newline ();
+
+  let formula2 = "((a -> (b -> c)) -> c) -> ((((a -> c) -> c) -> (((b -> c) -> c) -> c)) -> c)" in
+  let tree2 = Parser.parse_formula formula2 in
+
+
+  let seq1 = Formula.create_sequent [] [tree2] in
+
+  let output = "output.tex" in
+  let out = open_out output in
+  Formula.latex_header out;
+  
+  Formula.proof_search_latex out (fun i -> i) seq1 |> Printf.printf "%B\n";
+
+  Formula.latex_footer out;
+  close_out out;
+
+  Sys.command ("pdflatex " ^ output) |> Printf.printf "%d\n"
--- src/parser.ml
+++ src/parser.ml
@@ -1,8 +1,6 @@
-type operator = Impl | And | Or | Not
+type token = Var of string | OpenP | ClosedP | Impl | And | Or | Not
 
 let operator_names = [("->", Impl); ("&&", And); ("||", Or); ("!", Not)]
-
-type token = Operator of operator * int | Word of string * int | Parenthesis of bool * int
 
 exception No_such_element
 exception ParseException of string
@@ -44,53 +42,141 @@ let next_token string i operators =
 
   if j >= n then raise No_such_element;
 
-  if string.[j] = '(' then Parenthesis(true, j + 1)
-  else if string.[j] = ')' then Parenthesis(false, j + 1)
+  if string.[j] = '(' then (OpenP, j + 1)
+  else if string.[j] = ')' then (ClosedP, j + 1)
   else (
     let op = List.find_opt (fun (name, _) -> equals string name j) operators in
 
     match op with
-      | Some((op_name, op)) -> Operator(op, j + String.length op_name)
+      | Some((op_name, op)) -> (op, j + String.length op_name)
       | _ -> 
         let word = next_word string j operators in 
-        Word(word, j + String.length word)
+        (Var(word), j + String.length word)
   )
   
-let is_word token = match token with
-  | Word(_) -> true
+let is_var token = match token with
+  | Var(_) -> true
   | _ -> false
 
 let is_operator token = match token with
-  | Operator(_) -> true
+  | Impl | And | Or | Not -> true
   | _ -> false
 
 let is_word_opt token = match token with
-  | Some(Word(_)) -> true
+  | Some(Var(_)) -> true
   | _ -> false
 
 let is_operator_opt token = match token with
-  | Some(Operator(_)) -> true
+  | Some(token) -> is_operator token
   | _ -> false
+
+(* Assume that op1 and op2 are operators*)
+let has_higher_precedence op1 op2 = match op1, op2 with
+  | Impl, _   -> true   (* impl has priority over everything *)
+  | And, Impl -> false
+  | And, _    -> true   (* and has priority over and, or and not*)
+  | Or, Impl  -> false
+  | Or, And   -> false
+  | Or, _     -> true   (* or has priority over or and not*)
+  | Not, Not  -> true   (* not has priority over not*)
+  | _         -> false
+
+let rec push_operator operator stack output = 
+  if not (Stack.is_empty stack) then (
+    let op2 = Stack.top stack in
+
+    if is_operator op2 && has_higher_precedence op2 operator then (
+      Stack.push (Stack.pop stack) output;
+      push_operator operator stack output;
+    ) else (
+      Stack.push operator stack
+    )
+  ) else (
+    Stack.push operator stack
+  )
+
+let rec unpack_parenthesis stack output =
+  if Stack.is_empty stack then raise (ParseException "Invalid parenthesis")
+  else (
+    let token = Stack.pop stack in
+
+    if token <> OpenP then (
+      Stack.push token output;
+      unpack_parenthesis stack output;
+    )
+  )
+
+let print_token token = match token with
+  | And -> Printf.printf "&&"
+  | Or -> Printf.printf "||"
+  | Impl -> Printf.printf "->"
+  | Not -> Printf.printf "!"
+  | Var(var) -> Printf.printf "%s" var
+  | ClosedP -> Printf.printf ")"
+  | OpenP -> Printf.printf "("
+
+let print_stack stack printer =
+  Printf.printf "<";
+
+  stack |> Stack.to_seq
+        |> List.of_seq
+        |> List.rev
+        |> List.iter (fun f -> printer f; Printf.printf "; ")
+  ;
+
+  Printf.printf ">\n"
+
+let rec create_tree stack =
+  if Stack.is_empty stack then raise (ParseException "Empty stack")
+  else (
+    let token = Stack.pop stack in
+
+    match token with
+      | Var(a) -> Formula.V(a)
+      | Not -> Formula.Not(create_tree stack)
+      | And -> 
+        let right = create_tree stack
+        and left = create_tree stack in
+        Formula.And(left, right)
+      | Or -> 
+        let right = create_tree stack
+        and left = create_tree stack in
+        Formula.Or(left, right)
+      | Impl -> 
+        let right = create_tree stack
+        and left = create_tree stack in
+        Formula.Impl(left, right)
+      | _ -> raise (Invalid_argument "stack must only contains 'variables', 'not', 'and', 'or' and 'impl'")
+  )
 
 let parse_formula string = 
   let n = String.length string in
   let i = ref 0 in
 
-  (* stack of formula or parenthesis *)
   let stack = Stack.create () in
-  let last_token = ref None in
+  let output = Stack.create () in
 
   while !i < n do
-    let token = next_token string !i operator_names in
+    let (token, j) = next_token string !i operator_names in
 
-    match token with
-      | Parenthesis(_, j) -> i := j
-      | Word(word, j) -> i := j
-      | Operator(Impl, j) -> i := j
-      | Operator(And, j) -> i := j
-      | Operator(Or, j) -> i := j
-      | Operator(Not, j) -> i := j
-    ;
+    Printf.printf "%d - %d\n" j n;
+    print_stack stack print_token;
 
-    last_token := Some(token);
-  done
+    (match token with
+      | Var(_) -> Stack.push token output
+      | Impl | And | Or | Not -> push_operator token stack output
+      | OpenP -> Stack.push token stack
+      | ClosedP -> unpack_parenthesis stack output
+    );
+
+    i := j
+  done;
+
+  while not (Stack.is_empty stack) do
+    let token = Stack.pop stack in
+
+    if token = OpenP then raise (ParseException "Invalid parenthesis")
+    else Stack.push token output
+  done;
+
+  create_tree output
